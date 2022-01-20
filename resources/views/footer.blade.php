b4d6534cadd6c4943c4ada7ab1d815763c1830ac
Juwel-771 <juwelah99@gmail.com>
2022-01-21 01:31:35 +0600

    Project Set up

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="bg-light text-center p-4 text-lg-start">
+    <!-- Grid container -->
+    
+    <!-- Grid container -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">E-Commerce</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Brand</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Orders</a>
+        </li>
+        
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a class="nav-link" href="#">Cart(0)</a></li>
+      </ul>
+    </div>
+  </nav>
